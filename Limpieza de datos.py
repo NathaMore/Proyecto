69cbe3f08c92669a1b7f59d690cfe26e876884fb
Nathalia <na.morenom1@uniandes.edu.co>
2025-09-05 10:47:36 -0500

    Selección columnas y cálculo de fechas

--- Limpieza de datos.py
+++ Limpieza de datos.py
@@ -1,7 +1,27 @@
 import numpy as np
 import pandas as pd
 
-df = pd.read_csv("incident_event_log.csv")
+incidentes = pd.read_csv("incident_event_log.csv")
+incidentes["problem"] = incidentes["category"] + " - " + incidentes["subcategory"]
 
-print(df)
+#Selección de columnas
+columnas = ["number","incident_state", "active",
+             "reassignment_count", "reopen_count","sys_mod_count",
+             "contact_type", "location", "problem", "priority",
+             "assignment_group", "knowledge","opened_at", "resolved_at"             
+             ]
 
+df =incidentes[columnas]
+
+df = df[df["opened_at"].notna()]              # elimina NaN / None
+df = df[df["opened_at"].str.strip() != ""]    # elimina cadenas vacías
+df = df[df["opened_at"].str.strip() != "?"]   # elimina las que son "?"
+
+df = df[df["resolved_at"].notna()]
+df = df[df["resolved_at"].str.strip() != ""]
+df = df[df["resolved_at"].str.strip() != "?"]
+
+df["opened_at"] = pd.to_datetime(df["opened_at"], format="%d/%m/%Y %H:%M")
+df["resolved_at"] = pd.to_datetime(df["resolved_at"], format="%d/%m/%Y %H:%M")
+
+df["time"] = df["resolved_at"] - df["opened_at"]
